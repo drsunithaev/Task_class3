005d872c982bf086ba9a00b4f819d0e30ac9c987
drsunithaev <learnbeyondage@gmail.com>
2022-07-17 19:31:11 +0530

    testing the Git push operation

---
+++ test1.py
@@ -0,0 +1,16 @@
+import pymongo
+
+
+client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name" : "sunitha",
+    "email" : "sunitha@gmail.com",
+    "surname" : "ev"
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
